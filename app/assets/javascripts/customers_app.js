9bab5b6049ebe1d63d8d4c75365844a4568056fb
Salem Amba <salemamba@gmail.com>
2016-05-04 04:18:26 -0700

    implemented searching customers using angularjs,now

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -2,9 +2,13 @@ var app = angular.module('customers', []);
 
 app.controller('CustomerSearchController', ['$scope', '$http',
 	function ($scope, $http) {
+		var page = 0;
+
+		$scope.customers = [];
 		$scope.search = function(searchTerm) {
 			$http.get("/customers.json", 
-				     {"params": {"keywords": searchTerm}}
+				     {"params": {"keywords": searchTerm,
+				     "page": page}}
 		    ).then(function(response) {
 		    	$scope.customers = response.data;
 		    }, function(respsone) {
@@ -12,6 +16,18 @@ app.controller('CustomerSearchController', ['$scope', '$http',
 		    }
 		  );
 			
+		}
+
+		$scope.previousPage = function() {
+			if(page > 0) {
+				page = page - 1;
+				$scope.search($scope.keywords);
+			}
+		}
+
+		$scope.nextPage = function() {
+			page = page + 1;
+			$scope.search($scope.keywords);
 		}
 	}
 ]);
